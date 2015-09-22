8a1bb33b4e614531b219834fd25bca63f73c18b5
Elektropay <richie@elektropay.com>
2015-09-22 00:41:23 -0400

    Update auth0-variables.js

--- js/auth0-variables.js
+++ js/auth0-variables.js
@@ -1,3 +1,3 @@
 var AUTH0_DOMAIN = 'elektropay.auth0.com';
-var AUTH0_CLIENT_ID = 'jGMow0KO3WDJELW8XIxolqb1XIitjkYL';
-var AUTH0_DB_CONNECTION_NAME = 'Custom-Signup-Example-DB';
+var AUTH0_CLIENT_ID = 'S0KMdvy677ld1M0hzGk5vUt1KDbvTaDw';
+var AUTH0_DB_CONNECTION_NAME = 'elektropay-engine';
